affc6cb5fd36d865fa86615947528dc6eb2f0427
florianrabbah <floflo97231@live.fr>
2023-10-19 19:37:21 +0200

    zczc

---
+++ app.rb
@@ -0,0 +1,54 @@
+  require 'bundler'
+  Bundler.require
+
+  require_relative 'lib/app/board'
+
+  require_relative 'player'
+  require_relative 'game'
+  require_relative 'show'
+
+    #classe principale représentant l'application du jeu
+class Application
+ 
+  #methode pour demarrer le jeu 
+
+def start
+    puts "bienvenue dans le jeu du morpion !"
+     
+
+    puts "Bienvenue dans le jeu du morpion !"
+    print "Joueur 1, entrez votre prénom : "
+    player1_name = gets.chomp
+    print "Joueur 2, entrez votre prénom : "
+    player2_name = gets.chomp
+
+    # Crée un jeu avec les deux joueurs
+    game = Game.new(player1_name, player2_name)
+    # Crée un objet de la classe Show pour gérer l'affichage du jeu
+ Show.new(game)
+end 
+end
+
+#Lance l'application en appelant la méthode start
+App.new.start
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+      
